6e01ef0bc9c3c39b8a2deb5f8aa015a0dd1a2831
Visalbotr Chan <30560223+vchan14@users.noreply.github.com>
2024-03-09 22:21:47 +1300

    9March2024 courseinfo step9

--- part2/courseInfo/src/Course.jsx
+++ part2/courseInfo/src/Course.jsx
@@ -2,6 +2,10 @@ const Course = ({course}) => {
 
 	const {name, parts, id} = course;
 
+	const totalExercises = parts.reduce((acc, part) => {
+		return acc + part.exercises;
+	}, 0)
+
 	return (
 		<>
 			<h2 className="header">
@@ -13,6 +17,10 @@ const Course = ({course}) => {
 					{`${part.name} ${part.exercises}`}
 				</p>
 			)}
+
+			<h4>
+				{`total of ${totalExercises} exercises`}
+			</h4>
 
 		</>
 	)
