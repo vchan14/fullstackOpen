5271da2d8f46c133aabb46489de9d09281601f30
Visalbotr Chan <30560223+vchan14@users.noreply.github.com>
2024-03-10 12:09:47 +1300

    9March2024 phoneBook step1

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -4,21 +4,41 @@ const App = () => {
 	const [persons, setPersons] = useState([
 		{ name: 'Arto Hellas' }
 	])
-	const [newName, setNewName] = useState('')
+	const [newName, setNewName] = useState('');
+
+	const handleInputChange = (e) => {
+		const newString = e.target.value;
+		setNewName(newString);
+	}
+
+	const shownNames = () => {
+		return (persons.map(person => <div key={person.name}>{person.name}</div>));
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setPersons(persons.concat({name:newName}));
+		setNewName('');
+	}
 
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<form>
+			<form onSubmit={handleSubmit}>
 				<div>
-					name: <input />
+					name:
+					<input
+						value={newName}
+						onChange={handleInputChange}
+					/>
 				</div>
 				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			...
+			{shownNames()}
+
 		</div>
 	)
 }
