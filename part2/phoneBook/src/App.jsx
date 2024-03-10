0b74187982350e8c247be62459e4ab855c940fe1
Visalbotr Chan <30560223+vchan14@users.noreply.github.com>
2024-03-10 15:38:42 +1300

    10March2024 phoneBook step6

--- part2/phoneBook/src/App.jsx
+++ part2/phoneBook/src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import Filter from "./Filter.jsx";
 import PersonForm from "./PersonForm.jsx";
 import Persons from "./Persons.jsx";
+import axios from "axios";
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: 'Arto Hellas', number: '040-123456', id: 1 },
-		{ name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-		{ name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-		{ name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-	])
+	const [persons, setPersons] = useState([])
 	const [newName, setNewName] = useState('');
 	const [newNumber, setNewNumber] = useState('');
 	const [newSearch, setNewSearch] = useState('');
+
+	// example of axios and effect-hook
+	const hook = () => {
+		console.log('effect')
+		axios
+			.get('http://localhost:3001/persons')
+			.then(response => {
+				setPersons(response.data)
+			})
+	}
+	useEffect(hook, [])
 
 	const handleNewSearch = (e) => {
 		const newString = e.target.value;
