7b68568bfefa2861c41724440f8866202a03ed95
Visalbotr Chan <chanvisalbotr@gmail.com>
2024-04-30 22:19:29 +1200

    30April2024 part 4.5

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -4,7 +4,7 @@ const listHelper = require('../utils/list_helper')
 
 const blogs = [{
     _id: "5a422a851b54a676234d17f7",
-        title: "React patterns",
+    title: "React patterns",
     author: "Michael Chan",
     url: "https://reactpatterns.com/",
     likes: 7,
@@ -12,7 +12,7 @@ const blogs = [{
 },
 {
     _id: "5a422aa71b54a676234d17f8",
-        title: "Go To Statement Considered Harmful",
+    title: "Go To Statement Considered Harmful",
     author: "Edsger W. Dijkstra",
     url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
     likes: 5,
@@ -20,7 +20,7 @@ const blogs = [{
 },
 {
     _id: "5a422b3a1b54a676234d17f9",
-        title: "Canonical string reduction",
+    title: "Canonical string reduction",
     author: "Edsger W. Dijkstra",
     url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
     likes: 12,
@@ -36,7 +36,7 @@ const blogs = [{
 },
 {
     _id: "5a422ba71b54a676234d17fb",
-        title: "TDD harms architecture",
+    title: "TDD harms architecture",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
     likes: 0,
@@ -44,12 +44,23 @@ const blogs = [{
 },
 {
     _id: "5a422bc61b54a676234d17fc",
-        title: "Type wars",
+    title: "Type wars",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
     likes: 2,
     __v: 0
 }]
+
+const listWithOneBlog = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5,
+        __v: 0
+    }
+]
 
 
 test('dummy returns one', () => {
@@ -64,17 +75,6 @@ describe('total likes', () => {
         assert.strictEqual(result, 0)
     })
 
-    const listWithOneBlog = [
-        {
-            _id: '5a422aa71b54a676234d17f8',
-            title: 'Go To Statement Considered Harmful',
-            author: 'Edsger W. Dijkstra',
-            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-            likes: 5,
-            __v: 0
-        }
-    ]
-
     test('when list has only one blog, equals the likes of that', () => {
         const result = listHelper.totalLikes(listWithOneBlog)
         assert.strictEqual(result, 5)
@@ -84,4 +84,32 @@ describe('total likes', () => {
         const result = listHelper.totalLikes(blogs)
         assert.strictEqual(result, 36)
     })
+})
+
+
+describe('test favorite blog', () => {
+
+    test('of empty list is null', () => {
+        const result = listHelper.favoriteBlog([])
+        assert.strictEqual(result, null)
+    })
+
+    test('when list has only one blog, it is the favorite', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog)
+        assert.deepStrictEqual(result, {
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            likes: 5
+        })
+    })
+
+    test('of a bigger list is calculated right', () => {
+        const result = listHelper.favoriteBlog(blogs)
+        assert.deepStrictEqual(result, {
+            title: "Canonical string reduction",
+            author: "Edsger W. Dijkstra",
+            likes: 12
+        })
+    })
+
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -8,9 +8,27 @@ const totalLikes = (blogs) => {
     return blogs.reduce((a, b) => a + b.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    let currMax = 0;
+    let maxIndex = -1;
+    blogs.forEach((blog, i) => {
+        if (blog.likes >= currMax) {
+            currMax = blog.likes
+            maxIndex = i
+        }
+    })
+    if (maxIndex === -1) {
+        return null
+    }
+    const {title, author, likes} = blogs[maxIndex]
+
+    return {title, author, likes}
+}
+
 
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
