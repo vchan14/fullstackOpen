dc06a36de7d027d5fca9be69c5fd1627bca71b1f
Visalbotr Chan <visalbotr.chan@kaleris.com>
2024-06-02 21:37:38 +1200

    02June24 5.14

--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -34,3 +34,25 @@ test('Load blog component', async () => {
 		'Component testing is done with react-testing-library'
 	)
 })
+
+test('Show URL and likes when show button is clicked', async () => {
+
+	const blog = {
+		title: 'Component testing is done with react-testing-library',
+		author: 'Matti Luukkainen',
+		url: 'https://reactpatterns.com/',
+		likes: 7
+	}
+
+	const {container} = render(<Blog blog={blog} name='test'/>)
+	const button = screen.getByText('show')
+	button.click()
+
+
+	const urlElement = screen.getByText(/https:\/\/reactpatterns.com/i)
+	const likesElement = screen.getByText(/7/i)
+
+	expect(urlElement).toBeInTheDocument()
+	expect(likesElement).toBeInTheDocument()
+})
+
