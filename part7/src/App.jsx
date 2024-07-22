2b70543f04f36d652f722189684cb9df67bcac6b
Visalbotr Chan <chanvisalbotr@gmail.com>
2024-07-22 20:15:44 +1200

    7.1 add react router to the menu

--- part7/src/App.jsx
+++ part7/src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
 
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
+
+
 const Menu = () => {
   const padding = {
     paddingRight: 5
   }
   return (
-    <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
-    </div>
+      <div>
+        <Link href='#' style={padding} to="/">anecdotes</Link>
+        <Link href='#' style={padding} to="/create">create new</Link>
+        <Link href='#' style={padding} to="/about">about</Link>
+      </div>
   )
 }
 
@@ -125,10 +131,15 @@ const App = () => {
   return (
     <div>
       <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
+      <Router>
+        <Menu />
+        <Routes>
+          <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/create" element={<CreateNew addNew={addNew} />}></Route>
+        </Routes>
+      </Router>
+
       <Footer />
     </div>
   )
